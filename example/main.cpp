e6a5faf3712c4691699a4a4364784a0aa4092940
Vlach, Michael <mv185095@ncr.com>
2017-09-01 09:35:32 +0200

    Adjusted the example file.

--- example/main.cpp
+++ example/main.cpp
@@ -4,14 +4,14 @@ int main(int argc, char **argv)
 {
     cppcommandline::Parser commandLine;
 
-    int someoption = 0;
     std::string filename;
+    int someoption = 0;
     bool flag = false;
 
     commandLine.option().withDescription("A required filename").required().bindTo(filename);
     commandLine.option("someoption").asShortName("s").withDefaultValue(10).withDescription("My description of someoption").bindTo(someoption);
     commandLine.option("flag").withDefaultValue(false).withDescription("My flag").bindTo(flag);
-
     commandLine.parse(argc, argv);
+
     return 0;
 }
